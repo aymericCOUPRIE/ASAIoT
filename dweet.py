e8b798e93a1f411a35ec84268104aaaf32671c8b
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2020-01-12 02:37:38 +0100

    delete files

--- dweet.py
+++
@@ -1,5 +0,0 @@
-import requests
-import datetime
-curl "https://docs.google.com/forms/d/1oDpK3Sd6umR2XJdl_aBrGsGCVvqEP27ws3QNUYCtkS4/formResponse?ifq&entry.220205619=120&entry.2085367065=10&submit=Submit"
-
-// pour envoyer les donnees sur un google form dans mon drive
